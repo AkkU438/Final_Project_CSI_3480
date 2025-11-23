4705ad54a636d30bf9bfcad9fb0a518adf8cbd8b
AkkU438 <109703098+AkkU438@users.noreply.github.com>
2025-11-23 18:47:32 -0500

    Password Generator beta 1
    Created the skeleton of the password generator, no CSS styling to make it look good though

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,17 +1,58 @@
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
-let password = "";
-
-const length = 12;
-const array = new Uint32Array(length);
-window.crypto.getRandomValues(array);
-
-const p = document.getElementById("password");
+/*Alright so here are the values that we are gonna be using for the password*/ 
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
 
-for(let i = 0; i < length; i++){
+function generatePassword(length){
+    //Checks what buttons are clicked
+    const checkUpper = document.getElementById("value1").checked;
+    const checkLower = document.getElementById("value2").checked;
+    const checkNumber = document.getElementById("value3").checked;
+    const checkSymbols = document.getElementById("value4").checked;
+
+    /*Strings, kept inside cause 
+    when outside different passwords keep 
+    getting appended to it*/
+    let password = "";
+    let chars = "";
+
+    //Checks and appends certain strings
+    if(!checkUpper && !checkLower && !checkNumber && !checkSymbols){
+        chars += uppercase;
+    } else{
+        if(checkUpper) chars += uppercase;
+        if(checkLower) chars += lowercase;
+        if(checkNumber) chars += numbers;
+        if(checkSymbols) chars += symbols;
+    }
+
+
+    //Creates the new array to use for the password
+    const array = new Uint32Array(length);
+    window.crypto.getRandomValues(array);
+    
+    //For loop used to make the password
+    for(let i = 0; i < length; i++){
     const randIndex = array[i] % chars.length;
     password += chars[randIndex];
+    }
+    return password;
 }
 
-p.textContent = password;
-console.log(password);
+const slider = document.getElementById("slider");
+const output = document.getElementById("output");
+const p = document.getElementById("password");
+const gen = document.getElementById("generate");
+
+p.textContent = generatePassword(12);
+
+
+slider.addEventListener("input", ()=>{
+    output.textContent = slider.value;
+    p.textContent = generatePassword(slider.value);
+})
+gen.addEventListener("click", ()=> {
+    p.textContent = generatePassword(slider.value);
+})
